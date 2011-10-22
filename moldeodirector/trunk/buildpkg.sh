fc788144a64074c5ead9ed850bfbbd65a11db456
fabricio_costa <fabricio_costa@c963c68c-f84c-0410-a3d6-bef16c67260e>
2011-10-22 18:14:10 +0000

    ok

---
+++ moldeodirector/trunk/buildpkg.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+DIE=0
+
+echo "deb directory..."
+(mkdir deb ) > /dev/null || {
+	echo "cleaning..."
+
+	(rm -Rf deb/*) > /dev/null || {
+	  echo
+	  echo "**Error**: removing error ."
+	  echo ""
+	  exit 1
+	}
+}
+
+
+
+echo "making distribution source file..."
+(make dist) > /dev/null || {
+  echo
+  echo "**Error**: make dist ."
+  echo ""
+  exit 1
+}
+
+
+echo "copying..."
+
+(mv moldeodirector-*.tar.gz ./deb/) > /dev/null || {
+  echo
+  echo "**Error**: moving file ."
+  echo ""
+  exit 1
+}
+
+
+echo "renaming..."
+( rename 's/\.tar.gz/\.orig.tar.gz/' deb/*.tar.gz ) > /dev/null || {
+  echo
+  echo "**Error**: renaming to orig."
+  echo ""
+  exit 1
+}
+( rename 's/\-/\_/' deb/*.tar.gz ) > /dev/null || {
+  echo
+  echo "**Error**: renaming to underscore."
+  echo ""
+  exit 1
+}
+( rename 's/[A-Z]/[a-z/' deb/*.tar.gz ) > /dev/null || {
+  echo
+  echo "**Error**: renaming to lowercase."
+  echo ""
+  exit 1
+}
+
+echo "extracting..."
+
+(tar -C deb -zxvf ./deb/*.orig.tar.gz ) > /dev/null || {
+  echo
+  echo "**Error**: extracting ."
+  echo ""
+  exit 1
+}
+
+echo " 
+Now execute in deb folder:
+ cd deb/moldeodirector-*
+ dh_make -s -e fabricio.costa@moldeointeractive.com.ar
+ cp ../../control.amd64 debian/control
+ cp ../../rules.amd64 debian/rules
+ vi debian/changelog
+ dpkg-buildpackage -us -uc -rfakeroot 2>&1 | tee ../../buildpkg_logs.txt
+"
+
+
+echo "Success: extraction"
+exit 0
